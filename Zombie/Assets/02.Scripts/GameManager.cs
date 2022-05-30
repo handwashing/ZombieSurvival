9fb5f11ed60845cb6616c77a1b057e4d5b90093a
handwashing <99708666+handwashing@users.noreply.github.com>
2022-05-30 11:06:39 +0900

    20220530AM

--- Zombie/Assets/02.Scripts/GameManager.cs
+++ Zombie/Assets/02.Scripts/GameManager.cs
@@ -1,10 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+//점수와 게임오버 여부를 관리하는 게임 매니저
 public class GameManager : MonoBehaviour
-{
-    public static GameManager instance;
+{   //싱글턴 접근용 프로퍼티
+    public static GameManager instance
+    {
+        get
+        {
+            //만약 싱글턴 변수에 아직 오브젝트가 할당되지 않았다면...
+            if (m_instance == null)
+            {
+                //씬에서 GameManager 오브젝트를 찾아서 할당
+                
+            }
+        }
+    }
 
     public bool isGameover;
     // Start is called before the first frame update
